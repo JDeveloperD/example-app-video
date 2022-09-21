561360c11d2acd892d8ad2665a73465bc6441dc1
David Sandoval <ing.david1993@gmail.com>
2022-09-20 23:36:04 -0500

    add component input

--- src/components/inputs/Input/index.tsx
+++ src/components/inputs/Input/index.tsx
@@ -1,5 +1,32 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const Input = styled.input``
+const Input = styled.input<{
+  invalid?: boolean
+}>`
+  width: 100%;
+  background: transparent;
+  border-style: none;
+  background: ${({ theme }) => {
+    return theme.mode === 'light'
+    ? theme.colors.light
+    : theme.colors.black
+  }};
+  border: 2px solid transparent;
+  padding: 12px 1rem;
+  border-radius: 10px;
+  color: ${({ theme }) => theme.colors.textBody};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.textBody};
+  }
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary};
+  }
+
+  ${({ theme, invalid }) => invalid && css`
+    border-color: ${theme.colors.danger};
+  `}
+`
 
 export default Input
